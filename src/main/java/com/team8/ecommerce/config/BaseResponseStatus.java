4aae49c63e6eaf4efb628b3a66460272c0d3fc4f
DOWON LEE <domo7304@naver.com>
2021-11-25 21:12:13 +0900

    final

--- src/main/java/com/team8/ecommerce/config/BaseResponseStatus.java
+++ src/main/java/com/team8/ecommerce/config/BaseResponseStatus.java
@@ -29,7 +29,6 @@ public enum BaseResponseStatus {
     POST_USERS_EMPTY_EMAIL(false, 2015, "이메일을 입력해주세요."),
     POST_USERS_INVALID_EMAIL(false, 2016, "이메일 형식을 확인해주세요."),
     POST_USERS_EXISTS_EMAIL(false,2017,"중복된 이메일입니다."),
-    // -------추가한 것---------
     POST_USERS_EMPTY_USERNAME(false, 2018, "이름을 입력해주세요."),
     POST_USERS_EMPTY_NICKNAME(false, 2019, "닉네임을 입력해주세요."),
     POST_USERS_EXISTS_NICKNAME(false,2020,"중복된 닉네임입니다."),
